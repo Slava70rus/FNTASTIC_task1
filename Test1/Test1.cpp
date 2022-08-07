9f2aa08d8ab89d5c6f55b3728ae22d94784a27da
Slava70rus <55742266+Slava70rus@users.noreply.github.com>
2022-08-07 10:06:32 +0700

    Task1

---
+++ Test1/Test1.cpp
@@ -0,0 +1,54 @@
+﻿// Test1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int serch(string str, char temp)
+{
+    int count = 0 , len = str.length();
+
+    for (int i = 0; i < len; i++)
+    {
+        if (temp == str[i]) count++;
+
+    }
+
+    return count;
+
+}
+
+
+int main()
+{
+    string str;
+
+    vector<int> counts;
+
+    getline(cin , str);
+    
+    int len = str.length();
+
+    // приводим к нижнему регистру
+    transform(str.begin(), str.end(), str.begin(), tolower); 
+
+    // запоминаем дублирующиеся индексы
+    for (int i = 0; i < len; i++)
+    {
+        counts.push_back( serch(str, str[i]) ); 
+    }
+    
+
+    // производим замену
+    for (int i = 0; i < len; i++)
+    {
+        str[i] = counts[i] > 1 ? ')' : '(';
+    }
+
+    cout <<str;
+}
+
